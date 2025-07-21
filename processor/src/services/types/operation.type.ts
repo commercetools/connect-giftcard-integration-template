1c4312ff0e869cd4734d2d901e179963089d78d1
dasanor <david.sanchez@commercetools.com>
2025-07-21 16:46:29 +0200

    feat(payment-intents): Support linking refund requests to existing transactions.

--- processor/src/services/types/operation.type.ts
+++ processor/src/services/types/operation.type.ts
@@ -21,6 +21,7 @@ export type RefundPaymentRequest = {
   amount: AmountSchemaDTO;
   payment: Payment;
   merchantReference?: string;
+  transactionId?: string;
 };
 
 export type ReversePaymentRequest = {
